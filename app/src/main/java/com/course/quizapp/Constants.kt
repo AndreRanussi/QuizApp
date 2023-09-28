7eaf122943937985dbc528c3bedcea259681c6e9
Andre Ranussi <ranussi.a@gmail.com>
2023-09-28 17:41:08 +0100

    working on retrieving country flags

---
+++ app/src/main/java/com/course/quizapp/Constants.kt
@@ -0,0 +1,10 @@
+package com.course.quizapp
+object Constants {
+    fun getQuestions():ArrayList<Question> {
+        val questionsList = ArrayList<Question>()
+
+        return questionsList
+    }
+
+
+}
--- app/src/main/java/com/course/quizapp/MainActivity.kt
+++ app/src/main/java/com/course/quizapp/MainActivity.kt
@@ -1,13 +1,47 @@
 package com.course.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.view.WindowCompat
+import com.course.quizapp.CountrList.countries
+import com.google.android.material.textfield.TextInputEditText
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //APP SETTINGS
+        WindowCompat.setDecorFitsSystemWindows(
+            window, false
+        )
+
+        //APP FUNCTIONALITY
+        val btnStart: Button = findViewById(R.id.btnStart)
+        val etName: EditText = findViewById(R.id.etName)
+        var playerName: String = ""
+
+
+
+        btnStart.setOnClickListener {
+
+
+            if(etName.text.isNotEmpty()) {
+                playerName = etName.text.toString()
+                var country = countries[2]
+                println(country)
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+            }
+        }
 
     }
 }
